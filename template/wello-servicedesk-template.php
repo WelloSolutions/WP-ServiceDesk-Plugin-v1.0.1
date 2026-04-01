ec6a447d2e289432ffa5956b25f2bbde570681c6
Hemant Kumar <h.kumar@wello.solutions>
2026-04-01 15:36:56 +0530

    Add files via upload

---
+++ template/wello-servicedesk-template.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: Wello Service Desk
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+    <main id="root"></main>
+
+    <?php wp_footer(); ?>
+</body>
+</html>
